02cbb2c00428e04f192c778c2164add500816c18
Denner Augusto <63256030+denner-augusto@users.noreply.github.com>
2023-12-26 21:47:52 +0000

    Adicionar teste da função soma

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,8 @@
-test("teste do expect", () => {
-  expect(1).toBe(1);
+// Importando função calculadora do arquivo models/calculadora.js
+const calculadora = require("../models/calculadora");
+
+test("Espera-se que a soma de 2 + 2 seja 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+  console.log("O resultado é " + resultado);
 });
