8ddbd585d83aad45e0e9ae2f7c45b75a137cca15
denner-augusto <denner.augusto300@gmail.com>
2024-02-28 15:52:29 -0300

    Add /api/v1/status endpoint

--- models/calculadora.js
+++
@@ -1,5 +0,0 @@
-function somar(num1, num2) {
-  return num1 + num2;
-}
-// Para exportar a função de modo que todo o sistema consiga utilizar
-exports.somar = somar;
--- tests/calculadora.test.js
+++
@@ -1,8 +0,0 @@
-// Importando função calculadora do arquivo models/calculadora.js
-const calculadora = require("../models/calculadora");
-
-test("Espera-se que a soma de 2 + 2 seja 4", () => {
-  const resultado = calculadora.somar(2, 2);
-  expect(resultado).toBe(4);
-  console.log("O resultado é " + resultado);
-});
