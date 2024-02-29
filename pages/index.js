d96a1691d7cb9e0f71911e5404d7ed58fb99c1e6
denner-augusto <denner.augusto300@gmail.com>
2024-02-29 11:50:57 -0300

    Import for add the navbar to the website

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom-client";
+import App from "./app";
+import "./styles.css";
 function Home() {
   return <h1 align="center"> Em Construção </h1>;
 }
