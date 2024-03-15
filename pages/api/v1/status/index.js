fd90540f3ebfe617625c1cd05066d54b5a577e4a
denner-augusto <denner.augusto300@gmail.com>
2024-03-15 16:55:29 -0300

    Add Postgres version, max and active connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,18 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const dbParams = await database.query(
+    "SELECT version() as version, current_setting('max_connections') as max;",
+  );
+  const dbParamsActive = await database.query(
+    "SELECT p.pid, p.usename, p.state, count(*) as active FROM pg_stat_activity AS p WHERE p.state = 'active' GROUP BY 1,2,3;",
+  );
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: dbParams.rows[0].version,
+    max_connections: dbParams.rows[0].max,
+    active_connections: dbParamsActive.rows[0].active,
+  });
 }
 
 export default status;
